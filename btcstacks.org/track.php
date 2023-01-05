628daf95ecb037e76abb5f407f40592099f5c4ef
Nedev1 <122028181+Nedev1@users.noreply.github.com>
2023-01-05 18:31:08 +0200

    Add files via upload

---
+++ btcstacks.org/track.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TRACK ♦ ETHSTACKS</title>
+    <link rel="stylesheet" href="css/styles.css ">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,500;0,600;0,900;1,700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class='box'>
+    <div class='wave -one'></div>
+    <div class='wave -two'></div>
+    <div class='wave -three'></div>
+</div>
+
+<header>
+    <nav>
+        <ul>
+            <li><a href="./index.php#offer">SHOP</a></li>
+            <li><a href="./faq.php">FAQ</a></li>
+            <li><a href="./track.php">TRACK</a></li>
+            <li><a href="https://t.me/ethstacksnetproof" target="_blank">PROOF</a></li>
+        </ul>
+    </nav>
+</header>    <div class="track">
+        <h1>Track your cloned card</h1>
+        <div class="track-form">
+            <div class="track-panel">
+                <span class="track-holder"><input type="text" id="trackTxt" maxlength="18" placeholder="Enter your order number"></span>
+                <input type="button" id="trackBtn" value="Track">
+            </div>
+            <div id="output-holder"></div>
+        </div>
+    </div>
+</body>
+<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+<script src="track.js"></script>
+
+</html>
